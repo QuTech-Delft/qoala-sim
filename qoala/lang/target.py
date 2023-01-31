08229eb0501e8a514136f73b0901ae7d1792ca1a
Bart van der Vecht <b.vandervecht@tudelft.nl>
2023-01-31 14:22:12 +0100

    EHI dev

--- qoala/lang/target.py
+++ qoala/lang/target.py
@@ -1,4 +1,28 @@
+from dataclasses import dataclass
+from typing import Dict, List
+
+from netqasm.lang.instr.base import NetQASMInstruction
+from netqasm.lang.instr.flavour import Flavour
+
+
+@dataclass(eq=True, frozen=True)
+class ExposedQubitInfo:
+    is_communication: bool
+    decoherence_rate: float  # rate per second
+
+
+@dataclass(eq=True, frozen=True)
+class ExposedGateInfo:
+    ids: List[int]  # ordered list of qubit IDs
+    duration: int  # ns
+    decoherence: List[int]  # rate per second, per qubit ID (same order as `ids`)
+
+
+@dataclass(eq=True, frozen=True)
 class ExposedHardwareInfo:
     """Hardware made available to offline compiler."""
 
-    pass
+    qubit_infos: Dict[int, ExposedQubitInfo]  # qubit ID -> info
+
+    flavour: Flavour  # set of NetQASM instrs, no info about which qubits can do what instr
+    gate_infos: Dict[NetQASMInstruction, ExposedGateInfo]  # instr -> info
