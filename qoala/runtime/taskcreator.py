66e026b6a10b911bcfc185e5ea08ac5d84c8fe79
Bart van der Vecht <b.vandervecht@tudelft.nl>
2023-03-29 23:46:27 +0200

    Test taskcreator

--- qoala/runtime/taskcreator.py
+++ qoala/runtime/taskcreator.py
@@ -4,8 +4,12 @@ from dataclasses import dataclass
 from enum import Enum, auto
 from typing import List, Optional, Tuple
 
+from netqasm.lang.instr import core
+
+from qoala.lang.ehi import ExposedHardwareInfo, NetworkEhi
 from qoala.lang.hostlang import BasicBlockType, RunRequestOp, RunSubroutineOp
 from qoala.lang.program import IqoalaProgram
+from qoala.lang.routine import LocalRoutine
 
 
 class TaskExecutionMode(Enum):
@@ -25,10 +29,12 @@ class RoutineType(Enum):
     REQUEST = auto()
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class CpuTask:
     pid: int
     block_name: str
+    duration: Optional[float] = None
+    max_time: Optional[float] = None
 
 
 @dataclass
@@ -43,11 +49,13 @@ class CpuSchedule:
         return CpuSchedule(tasks=[(None, t) for t in tasks])
 
 
-@dataclass
+@dataclass(eq=True, frozen=True)
 class QpuTask:
     pid: int
     routine_type: RoutineType
     block_name: str
+    duration: Optional[float] = None
+    max_time: Optional[float] = None
 
 
 @dataclass
@@ -64,34 +72,83 @@ class TaskCreator:
         self._mode = mode
 
     def from_program(
-        self, program: IqoalaProgram, pid: int
+        self,
+        program: IqoalaProgram,
+        pid: int,
+        ehi: Optional[ExposedHardwareInfo] = None,
+        network_ehi: Optional[NetworkEhi] = None,
     ) -> Tuple[List[CpuTask], List[QpuTask]]:
         if self._mode == TaskExecutionMode.ROUTINE_ATOMIC:
-            return self._from_program_routine_atomic(program, pid)
+            return self._from_program_routine_atomic(program, pid, ehi, network_ehi)
         else:
             raise NotImplementedError
 
     def _from_program_routine_atomic(
-        self, program: IqoalaProgram, pid: int
+        self,
+        program: IqoalaProgram,
+        pid: int,
+        ehi: Optional[ExposedHardwareInfo] = None,
+        network_ehi: Optional[NetworkEhi] = None,
     ) -> Tuple[List[CpuTask], List[QpuTask]]:
         cpu_tasks: List[CpuTask] = []
         qpu_tasks: List[QpuTask] = []
 
         for block in program.blocks:
             if block.typ == BasicBlockType.HOST:
-                cputask = CpuTask(pid, block.name)
+                if ehi is not None:
+                    duration = ehi.latencies.host_instr_time * len(block.instructions)
+                else:
+                    duration = None
+                cputask = CpuTask(pid, block.name, duration)
                 cpu_tasks.append(cputask)
             elif block.typ == BasicBlockType.LR:
                 assert len(block.instructions) == 1
                 instr = block.instructions[0]
                 assert isinstance(instr, RunSubroutineOp)
-                qputask = QpuTask(pid, RoutineType.LOCAL, block.name)
+                if ehi is not None:
+                    routine = program.local_routines[instr.subroutine]
+                    duration = self._compute_lr_duration(ehi, routine)
+                else:
+                    duration = None
+                qputask = QpuTask(pid, RoutineType.LOCAL, block.name, duration)
                 qpu_tasks.append(qputask)
             elif block.typ == BasicBlockType.RR:
                 assert len(block.instructions) == 1
                 instr = block.instructions[0]
                 assert isinstance(instr, RunRequestOp)
-                qputask = QpuTask(pid, RoutineType.REQUEST, block.name)
+                if network_ehi is not None:
+                    # TODO: refactor!!
+                    epr_time = list(network_ehi.links.values())[0].duration
+                    routine = program.request_routines[instr.req_routine]
+                    duration = epr_time * routine.request.num_pairs
+                else:
+                    duration = None
+                qputask = QpuTask(pid, RoutineType.REQUEST, block.name, duration)
                 qpu_tasks.append(qputask)
 
         return cpu_tasks, qpu_tasks
+
+    def _compute_lr_duration(
+        self, ehi: ExposedHardwareInfo, routine: LocalRoutine
+    ) -> float:
+        duration = 0.0
+        # TODO: refactor this
+        for instr in routine.subroutine.instructions:
+            if (
+                type(instr)
+                in [
+                    core.SetInstruction,
+                    core.StoreInstruction,
+                    core.LoadInstruction,
+                    core.LeaInstruction,
+                ]
+                or isinstance(instr, core.BranchBinaryInstruction)
+                or isinstance(instr, core.BranchUnaryInstruction)
+                or isinstance(instr, core.JmpInstruction)
+                or isinstance(instr, core.ClassicalOpInstruction)
+                or isinstance(instr, core.ClassicalOpModInstruction)
+            ):
+                duration += ehi.latencies.qnos_instr_time
+            else:
+                duration += ehi.find_single_gate(0, type(instr)).duration
+        return duration
--- tests/test_taskcreator.py
+++ tests/test_taskcreator.py
@@ -1,6 +1,16 @@
 import os
 
+from netqasm.lang.instr import core
+
 from qoala.lang.parse import IqoalaParser
+from qoala.runtime.environment import NetworkInfo
+from qoala.runtime.lhi import (
+    LhiLatencies,
+    LhiLinkInfo,
+    LhiProcNodeInfo,
+    LhiTopologyBuilder,
+    NetworkLhi,
+)
 from qoala.runtime.taskcreator import (
     CpuTask,
     QpuTask,
@@ -8,6 +18,8 @@ from qoala.runtime.taskcreator import (
     TaskCreator,
     TaskExecutionMode,
 )
+from qoala.sim.build import build_network_from_lhi
+from qoala.sim.network import ProcNodeNetwork
 
 
 def relative_path(path: str) -> str:
@@ -33,5 +45,61 @@ def test1():
     ]
 
 
+def setup_network() -> ProcNodeNetwork:
+    topology = LhiTopologyBuilder.perfect_uniform_default_gates(num_qubits=3)
+    latencies = LhiLatencies(
+        host_instr_time=1000, qnos_instr_time=2000, host_peer_latency=3000
+    )
+    link_info = LhiLinkInfo.perfect(duration=20_000)
+
+    alice_lhi = LhiProcNodeInfo(
+        name="alice", id=0, topology=topology, latencies=latencies
+    )
+    network_lhi = NetworkLhi.fully_connected([0, 1], link_info)
+    network_info = NetworkInfo.with_nodes({0: "alice", 1: "bob"})
+    bob_lhi = LhiProcNodeInfo(name="bob", id=1, topology=topology, latencies=latencies)
+    return build_network_from_lhi([alice_lhi, bob_lhi], network_info, network_lhi)
+
+
+def test2():
+    network = setup_network()
+    alice = network.nodes["alice"]
+
+    path = relative_path("test_scheduling_alice.iqoala")
+    with open(path) as file:
+        text = file.read()
+    program = IqoalaParser(text).parse()
+
+    creator = TaskCreator(TaskExecutionMode.ROUTINE_ATOMIC)
+    pid = 3
+    cpu_tasks, qpu_tasks = creator.from_program(
+        program, pid, alice.local_ehi, alice.network_ehi
+    )
+
+    cpu_time = alice.local_ehi.latencies.host_instr_time
+
+    assert cpu_tasks == [
+        CpuTask(pid, "blk_host0", 2 * cpu_time),
+        CpuTask(pid, "blk_host1", 1 * cpu_time),
+        CpuTask(pid, "blk_host2", 1 * cpu_time),
+    ]
+
+    qpu_time = alice.local_ehi.latencies.qnos_instr_time
+    meas_time = alice.local_ehi.find_single_gate(0, core.MeasInstruction).duration
+    epr_time = alice.network_ehi.get_link(0, 1).duration
+    # assert qpu_tasks == [
+    expected = [
+        QpuTask(pid, RoutineType.LOCAL, "blk_add_one", 5 * qpu_time),
+        QpuTask(pid, RoutineType.REQUEST, "blk_epr_md_1", 1 * epr_time),
+        QpuTask(pid, RoutineType.REQUEST, "blk_epr_md_2", 2 * epr_time),
+        QpuTask(pid, RoutineType.REQUEST, "blk_epr_ck_1", 1 * epr_time),
+        QpuTask(pid, RoutineType.LOCAL, "blk_meas_q0", 3 * qpu_time + 1 * meas_time),
+        QpuTask(pid, RoutineType.REQUEST, "blk_epr_ck_2", 2 * epr_time),
+        QpuTask(pid, RoutineType.LOCAL, "blk_meas_q0_q1", 6 * qpu_time + 2 * meas_time),
+    ]
+    assert qpu_tasks == expected
+
+
 if __name__ == "__main__":
     test1()
+    test2()
