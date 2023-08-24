87b48608ab067ce1383a152a3c3394b96b9f45f4
Thomas Beauchamp <t.r.beauchamp@tudelft.nl>
2023-08-24 11:49:49 +0200

    added new event CUTOFF_REACHED

--- qoala/sim/events.py
+++ qoala/sim/events.py
@@ -2,6 +2,7 @@ from pydynaa import EventType
 
 EVENT_WAIT = EventType("SCHEDULER_WAIT", "scheduler wait")
 EPR_DELIVERY = EventType("EPR_DELIVERY", "EPR delivery")
+CUTOFF_REACHED = EventType("CUTOFF_REACHED", description="Entanglement generation cutoff reached")
 
 
 # Signals inside a single node
