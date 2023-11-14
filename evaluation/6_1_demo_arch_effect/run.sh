5fd0f1c792555d44d4c19572d6b86b2aaaa8ade3
Bart van der Vecht <b.vandervecht@tudelft.nl>
2023-11-14 17:57:31 +0100

    Run 6.1 1000 iterations

--- evaluation/6_1_demo_arch_effect/run.sh
+++ evaluation/6_1_demo_arch_effect/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-num_runs=10
+num_runs=1000
 
 # QKD
 num_pairs=100
@@ -8,8 +8,17 @@ num_pairs=100
 # BQC
 num_clients=1
 
+echo "Running GHZ"
 python ghz/eval_ghz.py -n $num_runs
+
+echo "Running Ping-Pong"
 python pingpong/eval_pingpong.py -n $num_runs
+
+echo "Running QKD"
 python qkd/eval_qkd.py -n $num_runs -p $num_pairs
+
+echo "Running Teleport"
 python teleport/eval_teleport.py -n $num_runs
+
+echo "Running VBQC"
 python vbqc/eval_vbqc.py -n $num_runs -c $num_clients
