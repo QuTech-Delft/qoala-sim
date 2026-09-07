e0f6ef5c73b038883e5492cc69c541cbecef7dd1
spoukke <sacha.bernheim@hey.com>
2026-09-07 11:37:53 +0200

    chore: fix mypy check

--- qoala/sim/scheduling/cpusched.py
+++ qoala/sim/scheduling/cpusched.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import copy
 import random
+import sys
 from abc import abstractmethod
 from typing import Dict, Generator, List, Optional, Tuple
 
@@ -371,7 +373,23 @@ class CpuScheduler(ProcessorScheduler):
                         waiting_msg_from_peer
                     )
 
-                    ev_expr = ev_msg_arrived | ev_expr
+                    if sys.version_info.minor > 10:
+                        # This is a workaround for a python 3.11+ change of behavior
+                        # In some release of Python 3.11, there was a change how the NotImplemented
+                        # is interpreted in boolean context evaluation
+                        # (see https://docs.python.org/3.13/library/constants.html#NotImplemented).
+                        # pydyna.core.EventExpression has a __or__ (and __ror__) operator implemented
+                        # however, they raise NotImplemented when the other operand is None.
+                        # In Python 3.10, this still evaluates to "true" in boolean contexts, but
+                        # in Python 3.11+, this raises a TypeError, which makes this fail.
+                        # As a workaround, we make a shallow copy of ev_expr when ev_msg_arrived is None.
+                        # This is the equivalent behavior in Python 3.10
+                        if ev_msg_arrived is None:
+                            ev_expr = copy.copy(ev_expr)
+                        else:
+                            ev_expr = ev_msg_arrived | ev_expr
+                    else:
+                        ev_expr = ev_msg_arrived | ev_expr
                     yield ev_expr
                     if len(ev_expr.first_term.triggered_events) > 0:
                         # It was "ev_msg_arrived" that triggered.
